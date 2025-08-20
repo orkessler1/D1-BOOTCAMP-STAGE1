3e5736ece0b0f811a6038927564ad7af7f491b92
orkessler1 <orkessler1@gmail.com>
2025-08-20 13:31:53 +0300

    finished xp

---
+++ HTML-Intermediate/week1/day1/day1-exercise.py
@@ -0,0 +1,36 @@
+#exercise1
+print('hello world'*4)
+#exercise2
+print(99*99*99*8)
+#exercise4
+computer_brand = (input('what is your computer name'))
+print(f'i have a {computer_brand} computer')
+#exercise5
+name = input('what is your name')
+age = input('what is your age')
+shoe_size = input('what is your shoe size')
+info = (f'my name is {name} im {age} years old and my shoe size is {shoe_size}')
+print(info)
+
+#exercise6
+a = int(input('pick a number'))
+b = int(input('pick a number'))
+if a > b :
+    print('hello world')
+#exercise7
+number = int(input('pick a number'))
+if number % 2 == 0 :
+    print('even')
+else :
+    print('odd')
+#exercise8
+name = input('what is your name')
+myname = 'ori'
+if name == myname :
+    print('ha ha we have the same name')
+#exercise9
+height = int(input('what is your height'))
+if height > 145 :
+    print('you are tall enough')
+else :
+    print('grow some more')
